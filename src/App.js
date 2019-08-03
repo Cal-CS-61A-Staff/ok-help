701ae99cbe2d52f4d33424647da848330e99487e
rahularya <rahularya@berkeley.edu>
2019-08-02 23:10:50 -0700

    Added dependencies.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scores: [],
+            plannedScores: [],
+            future: false,
+        };
+    }
+
+    render() {
+        return (
+            <div className="App container">
+                <div className="row">
+                    <div className="col">
+                        <br />
+                        <h1 className="display-4">
+                            <strong>okpy</strong>
+                            {" "}
+                            Command Generator
+                        </h1>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
