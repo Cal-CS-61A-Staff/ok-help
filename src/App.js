530c32c89313774c343585bd1f30212181f915d1
rahularya <rahularya@berkeley.edu>
2019-08-02 23:34:00 -0700

    More nonsense.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "./App.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './App.css';
+import GeneratedCommand from './GeneratedCommand.js';
 
 function App() {
-    return (
-        <div className="App container">
-            <div className="row">
-                <div className="col">
-                    <br/>
-                    <h1 className="display-4">
-                        <strong>okpy</strong>
-                        {" "}
-                        Command Generator
-                    </h1>
-
-                </div>
-            </div>
+  return (
+    <div className="App container">
+      <div className="row">
+        <div className="col">
+          <br/>
+          <h1 className="display-4">
+            <strong>okpy</strong>
+            {' '}
+            Command Generator
+          </h1>
+          <GeneratedCommand generatedCommand="python ok -q 03"/>
+          {/*<CommandOptions />*/}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/GeneratedCommand.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './GeneratedCommand.css';
+
+export default function GeneratedCommand(props) {
+  return <div className="GeneratedCommand">{props.generatedCommand}</div>;
+}
