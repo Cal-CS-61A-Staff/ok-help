561db2f1a140f47f99a86c9cf71314ae1622c617
rahularya <rahularya@berkeley.edu>
2019-08-03 14:10:56 -0700

    Small UI tweaks

---
+++ docs/build/precache-manifest.51197744613964c98f303834d94071cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac88b12429e12bc6cd0fc33dcc9cffd1",
+    "url": "/ok-help/index.html"
+  },
+  {
+    "revision": "30eb1828b233a06caaaf",
+    "url": "/ok-help/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "91916f0f2ea610ca84f7",
+    "url": "/ok-help/static/css/main.b3f6774b.chunk.css"
+  },
+  {
+    "revision": "30eb1828b233a06caaaf",
+    "url": "/ok-help/static/js/2.d412c00f.chunk.js"
+  },
+  {
+    "revision": "91916f0f2ea610ca84f7",
+    "url": "/ok-help/static/js/main.3cdf0838.chunk.js"
+  },
+  {
+    "revision": "9523f6011cb80b2b9491",
+    "url": "/ok-help/static/js/runtime~main.0e469500.js"
+  }
+]);
--- src/GeneratedCommand.js
+++ src/GeneratedCommand.js
@@ -1,55 +1,55 @@
 import React, { useState } from "react";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import "./GeneratedCommand.css";
 
 export default function GeneratedCommand(props) {
+    const [copyText, setCopyText] = useState("Copy");
 
-  const [copyText, setCopyText] = useState('Copy');
-    
-  let generated = "python3 ok";
-  if (props.options) {
-      for (const option of props.options.mandatoryArgs) {
-          if (option.shortForm) {
-              generated += ` -${option.shortForm}`;
-          } else {
-              generated += ` --${option.longForm}`;
-          }
-      }
-      for (const option of props.options.optionalArgs) {
-          if (props.selectedArgs[option.longForm]) {
-              if (option.shortForm) {
-                  generated += ` -${option.shortForm}`;
-              } else {
-                  generated += ` --${option.longForm}`;
-              }
-              if (option.isValue) {
-                  generated += ` ${props.selectedArgs[option.longForm]}`;
-              }
-          }
-      }
-  }
+    let generated = "python3 ok";
+    if (props.options) {
+        for (const option of props.options.mandatoryArgs) {
+            if (option.shortForm) {
+                generated += ` -${option.shortForm}`;
+            } else {
+                generated += ` --${option.longForm}`;
+            }
+        }
+        for (const option of props.options.optionalArgs) {
+            if (props.selectedArgs[option.longForm]) {
+                if (option.shortForm) {
+                    generated += ` -${option.shortForm}`;
+                } else {
+                    generated += ` --${option.longForm}`;
+                }
+                if (option.isValue) {
+                    generated += ` ${props.selectedArgs[option.longForm]}`;
+                }
+            }
+        }
+    }
 
-  return (
-    <div>
-      <div className="GeneratedCommand">
-        <code>
-          {generated}
-        </code>
-        <button className="fileNameSubmitBtn" type="button">
-          <CopyToClipboard 
-             text={generated}
-             onCopy={() => {
-               setCopyText('Copied!');
-               setTimeout(() => {
-                  setCopyText('Copy');
-                }, 1000);
-                document.activeElement.blur();
-             }}>
-             <span>{copyText}</span>
-          </CopyToClipboard>
-        </button>
-       </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="GeneratedCommand">
+                <code>
+                    {generated}
+                </code>
+                <CopyToClipboard
+                    text={generated}
+                    onCopy={() => {
+                        setCopyText("Copied!");
+                        setTimeout(() => {
+                            setCopyText("Copy");
+                        }, 1000);
+                        document.activeElement.blur();
+                    }}
+                >
+                    <button className="btn btn-primary fileNameSubmitBtn" type="button">
+                        <span>{copyText}</span>
+                    </button>
+                </CopyToClipboard>
+            </div>
+        </div>
+    );
 }
