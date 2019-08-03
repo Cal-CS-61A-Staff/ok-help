e53e7ad95422f5d0b38dcd67ba031eb131f39f00
rahularya <rahularya@berkeley.edu>
2019-08-02 23:22:48 -0700

    Administrivia

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scores: [],
-            plannedScores: [],
-            future: false,
-        };
-    }
+function App() {
+    return (
+        <div className="App container">
+            <div className="row">
+                <div className="col">
+                    <br/>
+                    <h1 className="display-4">
+                        <strong>okpy</strong>
+                        {" "}
+                        Command Generator
+                    </h1>
 
-    render() {
-        return (
-            <div className="App container">
-                <div className="row">
-                    <div className="col">
-                        <br />
-                        <h1 className="display-4">
-                            <strong>okpy</strong>
-                            {" "}
-                            Command Generator
-                        </h1>
-
-                    </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default App;
