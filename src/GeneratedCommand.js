50e9a2996c32180bed27644d73c671df2c48d520
Rahul Arya <rahularya@berkeley.edu>
2019-08-03 18:45:48 -0700

    Sticky top (#4)
    * Demo - DO NOT MERGE
    
    * Improved behavior.

--- src/GeneratedCommand.js
+++ src/GeneratedCommand.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import $ from "jquery";
 
 import { Textfit } from "react-textfit";
 
@@ -7,6 +8,9 @@ import "./GeneratedCommand.css";
 
 export default function GeneratedCommand(props) {
     const [copyText, setCopyText] = useState("Copy");
+
+    const sensor = useRef();
+    const ref = useRef();
 
     let generated = "python3 ok";
     if (props.options) {
@@ -31,31 +35,52 @@ export default function GeneratedCommand(props) {
         }
     }
 
+    function stickTop() {
+        const windowTop = $(window).scrollTop();
+        const { top } = $(sensor.current).offset();
+        if (windowTop > top) {
+            $(sensor.current).height($(ref.current).outerHeight());
+            $(ref.current).addClass("sticky");
+        } else {
+            $(ref.current).removeClass("sticky");
+            $(sensor.current).height(0);
+        }
+    }
+
+    $(window).scroll(stickTop);
+
     return (
-        <div className="GeneratedCommand">
-            <div className="Command">
-                <Textfit mode="single" forceSingleModeWidth={false} max={28}>
-                    <code>
-                        {generated}
-                    </code>
-                </Textfit>
+        <>
+            <div ref={sensor} />
+            <div className="row" ref={ref}>
+                <div className="col">
+                    <div className="GeneratedCommand">
+                        <div className="Command">
+                            <Textfit mode="single" forceSingleModeWidth={false} max={28}>
+                                <code>
+                                    {generated}
+                                </code>
+                            </Textfit>
+                        </div>
+                        <div className="CopyButtonContainer">
+                            <CopyToClipboard
+                                text={generated}
+                                onCopy={() => {
+                                    setCopyText("Copied!");
+                                    setTimeout(() => {
+                                        setCopyText("Copy");
+                                    }, 1000);
+                                    document.activeElement.blur();
+                                }}
+                            >
+                                <button className="btn btn-primary CopyButton" type="button">
+                                    <span>{copyText}</span>
+                                </button>
+                            </CopyToClipboard>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="CopyButtonContainer">
-                <CopyToClipboard
-                    text={generated}
-                    onCopy={() => {
-                        setCopyText("Copied!");
-                        setTimeout(() => {
-                            setCopyText("Copy");
-                        }, 1000);
-                        document.activeElement.blur();
-                    }}
-                >
-                    <button className="btn btn-primary CopyButton" type="button">
-                        <span>{copyText}</span>
-                    </button>
-                </CopyToClipboard>
-            </div>
-        </div>
+        </>
     );
 }
