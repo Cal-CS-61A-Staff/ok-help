eb4128b37b6183d34962fff1340ae6b7225b4475
Manav Rathod <manav.rathod@berkeley.edu>
2019-08-05 21:28:58 -0700

    Migrating Category.css to Category.js WIP

--- src/Category.js
+++ src/Category.js
@@ -3,6 +3,30 @@ import "./Category.css";
 import CheckBox from "./CheckBox.js";
 import ValueField from "./ValueField.js";
 
+const styles = {
+    activated: {
+        boxShadow: "0 5px 15px rgba(0, 0, 0, 0.3)",
+    },
+
+    category: {
+        border: "1px solid gray",
+        marginTop: "10px",
+        padding: '10px',
+        boxShadow: "0 1px 2px rgba(0, 0, 0, 0.15)",
+        transition: "opacity 0.2s ease-in",
+    },
+
+    deactivated: {
+        opacity: 0.5,
+    },
+
+    'deactivated:hover': {
+        cursor: "pointer",
+        /*box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3),*/
+        opacity: 1,
+    },
+};
+
 export default function Category(props) {
     const checkBoxes = props.category.optionalArgs.map((arg, index) => {
         if (arg.isValue) {
@@ -24,11 +48,13 @@ export default function Category(props) {
         }
     });
 
-    const className = `Category ${props.active ? "activated" : "deactivated"}`;
+    const activeStyle = props.active ? styles.activated : styles.deactivated;
+
+    const className = `${props.active ? "activated" : "deactivated"}`;
 
     return (
         <div className="col-lg-3">
-            <div className={className} onClick={props.onClick}>
+            <div className={className} style={styles.category} onClick={props.onClick}>
                 <h3>
                     {props.category.name}
                 </h3>
