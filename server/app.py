990ba36c93c2dbd7d716f9890927b83b95beeb2a
rahularya <rahularya@berkeley.edu>
2020-02-05 14:48:33 -0800

    added ok help

---
+++ server/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+@app.route("/")
+def index():
+	return send_from_directory("static", "index.html")
+
+if __name__ == "__main__":
+	app.run()
